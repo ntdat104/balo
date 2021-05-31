db0cb5d9c127fb7aea59aae7e44f9c39c71fdbe7
ntdat104 <ntd.k108cva@gmail.com>
2021-05-31 15:12:48 +0700

    update check_new_item & get_list_posts

--- test/check_new_item.test.js
+++ test/check_new_item.test.js
@@ -14,38 +14,57 @@ describe('API check_new_item', () => {
         let id = null;
         it('Lấy token thành công!', (done) => {
             const input = {
-                phonenumber: '0987654321',
+                phonenumber: '0123456789',
                 password: '123456',
             };
             chai.request(API_URL)
                 .post('/login')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     token = res.body.token;
                     done();
                 });
         });
-        it('Đăng bài thành công!', (done) => {
+        it('Đăng bài thành công', (done) => {
             const input = {
                 token,
-                described: 'Test đăng bài mới nhất',
+                described: 'Đây là bài viết thứ tư',
             };
             chai.request(API_URL)
                 .post('/add_post')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     res.body.should.have.property('data');
                     id = res.body.data.id;
                     done();
                 });
         });
-        it('Số phần tử mới chưa được hiển thị.', (done) => {
+        it('Lấy bài viết thành công', (done) => {
+            const post = {
+                token,
+                id,
+            };
+            chai.request(API_URL)
+                .post('/get_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have.property('data');
+                    done();
+                });
+        });
+        it('Số phần tử mới', (done) => {
             const input = {
                 last_id: id,
                 category_id: '0',
@@ -54,23 +73,25 @@ describe('API check_new_item', () => {
                 .post('/check_new_item')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     res.body.should.have.property('data');
                     done();
                 });
         });
-        it('Giá trị category_id không hợp lệ!', (done) => {
+        it('Last_id không hợp lệ', (done) => {
             const input = {
-                last_id: id,
-                category_id: '4',
+                last_id: "sbsjbssjjs",
+                category_id: '0',
             };
             chai.request(API_URL)
                 .post('/check_new_item')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1004);
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
                     res.body.should.be.a('object');
                     res.body.should.have
                         .property('message')
@@ -78,16 +99,17 @@ describe('API check_new_item', () => {
                     done();
                 });
         });
-        it('Giá trị category_id không hợp lệ!', (done) => {
+        it('Last_id không hợp lệ', (done) => {
             const input = {
-                last_id: id,
-                category_id: '-1',
+                last_id: ["apple", "banana", "lemon"],
+                category_id: '0',
             };
             chai.request(API_URL)
                 .post('/check_new_item')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1004);
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
                     res.body.should.be.a('object');
                     res.body.should.have
                         .property('message')
@@ -95,20 +117,127 @@ describe('API check_new_item', () => {
                     done();
                 });
         });
-        it('Giá trị last_id không tồn tại!', (done) => {
+        it('Last_id không hợp lệ', (done) => {
             const input = {
-                last_id: 'sjsnskass',
-                category_id: '2',
+                last_id: "abc",
+                category_id: '0',
             };
             chai.request(API_URL)
                 .post('/check_new_item')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(9992);
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
                     res.body.should.be.a('object');
                     res.body.should.have
                         .property('message')
-                        .eql('Post is not existed');
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Last_id không hợp lệ', (done) => {
+            const input = {
+                category_id: '0',
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Last_id không hợp lệ', (done) => {
+            const input = {
+                last_id: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                category_id: '0',
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Category_id không hợp lệ', (done) => {
+            const input = {
+                last_id: id,
+                category_id: 'sbsjbssjjs',
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Category_id không hợp lệ', (done) => {
+            const input = {
+                last_id: id,
+                category_id: ["apple", "banana", "lemon"],
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Category_id không hợp lệ', (done) => {
+            const input = {
+                last_id: id,
+                category_id: "abc",
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
+                    done();
+                });
+        });
+        it('Category_id không hợp lệ', (done) => {
+            const input = {
+                last_id: id,
+            };
+            chai.request(API_URL)
+                .post('/check_new_item')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.have.property('code').eql('1004');
+                    res.body.should.be.a('object');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Parameter value is invalid');
                     done();
                 });
         });
--- test/get_list_posts.test.js
+++ test/get_list_posts.test.js
@@ -14,31 +14,33 @@ describe('API get_list_posts', () => {
         let id = null;
         it('Lấy token thành công!', (done) => {
             const input = {
-                phonenumber: '0987654321',
+                phonenumber: '0123456789',
                 password: '123456',
             };
             chai.request(API_URL)
                 .post('/login')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     token = res.body.token;
                     done();
                 });
         });
-        it('Đăng bài thành công!', (done) => {
+        it('Đăng bài thành công', (done) => {
             const input = {
                 token,
-                described: 'Test đăng bài mới nhất',
+                described: 'Đây là bài viết thứ ba',
             };
             chai.request(API_URL)
                 .post('/add_post')
                 .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     res.body.should.have.property('data');
                     id = res.body.data.id;
@@ -56,8 +58,9 @@ describe('API get_list_posts', () => {
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
                     res.body.should.have.property('message').eql('OK');
                     res.body.should.have.property('data');
                     done();
@@ -74,8 +77,9 @@ describe('API get_list_posts', () => {
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(9998);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
                     res.body.should.have
                         .property('message')
                         .eql('Token is invalid');
@@ -84,7 +88,7 @@ describe('API get_list_posts', () => {
         });
         it('Token không hợp lệ!', (done) => {
             const post = {
-                token: '',
+                token: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.KTMtjpE5RsmxwEaDF_cDAelomv1QPTRu_t5sdGYZ2Bw',
                 lastid: id,
                 index: '2',
                 count: '10',
@@ -93,15 +97,160 @@ describe('API get_list_posts', () => {
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(9998);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
                     res.body.should.have
                         .property('message')
                         .eql('Token is invalid');
                     done();
                 });
         });
-        it('Tham không hợp lệ!', (done) => {
+        it('Token không hợp lệ!', (done) => {
+            const post = {
+                token: ["apple", "banana", "lemon"],
+                lastid: id,
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ!', (done) => {
+            const post = {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                lastid: id,
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ!', (done) => {
+            const post = {
+                lastid: id,
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: ["apple", "banana", "lemon"],
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: "",
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: () => {
+                    let string = "",
+                    for (let i = 0; i < 100000; i++) {
+                        string += i;
+                    }
+                    return string;
+                },
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ!', (done) => {
+            const post = {
+                token,
+                index: '2',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('index không hợp lệ!', (done) => {
             const post = {
                 token,
                 lastid: id,
@@ -112,15 +261,55 @@ describe('API get_list_posts', () => {
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(1004);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
                     res.body.should.have
                         .property('message')
-                        .eql('Parameter value is invalid');
+                        .eql('index is invalid');
                     done();
                 });
         });
-        it('Tham không hợp lệ!', (done) => {
+        it('index không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: id,
+                index: '',
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('index is invalid');
+                    done();
+                });
+        });
+        it('index không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: id,
+                count: '10',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('index is invalid');
+                    done();
+                });
+        });
+        it('count không hợp lệ!', (done) => {
             const post = {
                 token,
                 lastid: id,
@@ -131,30 +320,51 @@ describe('API get_list_posts', () => {
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(1004);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
                     res.body.should.have
                         .property('message')
-                        .eql('Parameter value is invalid');
+                        .eql('count is invalid');
                     done();
                 });
         });
-        it('Tham không hợp lệ!', (done) => {
+        it('count không hợp lệ!', (done) => {
             const post = {
                 token,
                 lastid: id,
-                index: '-1',
-                count: '-1',
+                index: '2',
+                count: '',
             };
             chai.request(API_URL)
                 .post('/get_list_posts')
                 .send(post)
                 .end((err, res) => {
-                    res.should.have.status(1004);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
                     res.body.should.have
                         .property('message')
-                        .eql('Parameter value is invalid');
+                        .eql('count is invalid');
+                    done();
+                });
+        });
+        it('count không hợp lệ!', (done) => {
+            const post = {
+                token,
+                lastid: id,
+                index: '2',
+            };
+            chai.request(API_URL)
+                .post('/get_list_posts')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have
+                        .property('message')
+                        .eql('count is invalid');
                     done();
                 });
         });
