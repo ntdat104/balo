cfcda7272ef2d7c973e65b16ed76585905d4c08d
ntdat104 <ntd.k108cva@gmail.com>
2021-05-31 15:40:40 +0700

    update

--- test/delete_post.test.js
+++ test/delete_post.test.js
@@ -10,18 +10,227 @@ const { API_URL } = require('../apiURL');
 
 describe('API delete_post', () => {
     describe('Method=POST /delete_post', () => {
-        it('Lấy bài viết thành công', (done) => {
-            const deletePost = {
-                token: 'sjjsnannsk',
-                id: 'anskndhsjjnsd',
+        let token = null;
+        let id = null;
+        it('Lấy token thành công!', (done) => {
+            const input = {
+                phonenumber: '0123456789',
+                password: '123456',
+            };
+            chai.request(API_URL)
+                .post('/login')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    token = res.body.token;
+                    done();
+                });
+        });
+        it('Đăng bài thành công', (done) => {
+            const input = {
+                token,
+                described: 'Đây là bài viết thứ năm',
+            };
+            chai.request(API_URL)
+                .post('/add_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have.property('data');
+                    id = res.body.data.id;
+                    done();
+                });
+        });
+        it('Xóa bài đăng thành công', (done) => {
+            const input = {
+                token,
+                id,
             };
             chai.request(API_URL)
                 .post('/delete_post')
-                .send(deletePost)
+                .send(input)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('message');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have.property('data');
+                    id = res.body.data.id;
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const input = {
+                token: "sbsjbssjjs",
+                id,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const input = {
+                token: ["apple", "banana", "lemon"],
+                id,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const input = {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                id,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const input = {
+                id,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const input = {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.KTMtjpE5RsmxwEaDF_cDAelomv1QPTRu_t5sdGYZ2Bw",
+                id,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const input = {
+                token,
+                id: "abcskj123js",
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const input = {
+                token,
+                id: "",
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const input = {
+                token,
+                id: "abcskj123js",
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const input = {
+                token,
+            };
+            chai.request(API_URL)
+                .post('/delete_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
                     done();
                 });
         });
--- test/edit_post.test.js
+++ test/edit_post.test.js
@@ -10,23 +10,236 @@ const { API_URL } = require('../apiURL');
 
 describe('API edit_post', () => {
     describe('Method=POST /edit_post', () => {
-        it('Lấy bài viết thành công', (done) => {
-            const editPost = {
-                token: 'sjjsnannsk',
-                id: 'anskndhsjjnsd',
-                described: 'sbsvs',
-                image: ['ảnh 1', 'ảnh 2', 'ảnh 3'],
-                image_del: ['ảnh x', 'ảnh y'],
-                image_sort: [2, 0, 1],
-                video: 'url video',
+        let token = null;
+        let id = null;
+        it('Lấy token thành công!', (done) => {
+            const input = {
+                phonenumber: '0123456789',
+                password: '123456',
+            };
+            chai.request(API_URL)
+                .post('/login')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    token = res.body.token;
+                    done();
+                });
+        });
+        it('Đăng bài thành công', (done) => {
+            const input = {
+                token,
+                described: 'Đây là bài viết thứ tư',
+            };
+            chai.request(API_URL)
+                .post('/add_post')
+                .send(input)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have.property('data');
+                    id = res.body.data.id;
+                    done();
+                });
+        });
+        it('Sửa bài đăng thành công', (done) => {
+            const post = {
+                token,
+                id,
+                described: "Mô tả mới"
             };
             chai.request(API_URL)
                 .post('/edit_post')
-                .send(editPost)
+                .send(post)
                 .end((err, res) => {
-                    res.should.have.status(1000);
+                    res.should.have.status(200);
                     res.body.should.be.a('object');
-                    res.body.should.have.property('message');
+                    res.body.should.have.property('code').eql('1000');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have.property('data');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const post = {
+                token: "sbsjbssjjs",
+                id,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const post = {
+                token: ["apple", "banana", "lemon"],
+                id,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const post = {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9",
+                id,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const post = {
+                id,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('Token không hợp lệ', (done) => {
+            const post = {
+                token: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.KTMtjpE5RsmxwEaDF_cDAelomv1QPTRu_t5sdGYZ2Bw",
+                id,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('Token is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const post = {
+                token,
+                id: ["apple", "mango", "banana"],
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const post = {
+                token,
+                id: "",
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const post = {
+                token,
+                id: "abcskj123js",
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
+                    done();
+                });
+        });
+        it('id không hợp lệ', (done) => {
+            const post = {
+                token,
+                described: "Mô tả mới"
+            };
+            chai.request(API_URL)
+                .post('/edit_post')
+                .send(post)
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    res.body.should.be.a('object');
+                    res.body.should.have.property('code').eql('9998');
+                    res.body.should.have.property('message').eql('OK');
+                    res.body.should.have
+                        .property('message')
+                        .eql('id is invalid');
                     done();
                 });
         });
